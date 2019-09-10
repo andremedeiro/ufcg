591c97be77e5e0df6877cfa71f5efcae0868da73
andremedeiro <andre.lucas.medeir@gmail.com>
2019-09-09 21:37:26 -0300

    Adicionado os arquivos das questoes do PC local

---
+++ prog1/exercicios/3Unidade/Telefonia_AndreLucas/AndreLucas_Telefonia.py
@@ -0,0 +1,17 @@
+# UFCG - Programação 1 - 2019.2
+# André Lucas Mederios Martins - 119210592
+# Telefonia
+
+minutos = int(input())
+
+taxa_ligacao = 1
+
+if minutos <= 3:
+    taxa_ligacao += minutos * 0.50
+else:
+    taxa_ligacao += minutos//5 * 3
+    minutos -= minutos//5 * 5
+    taxa_ligacao += minutos*0.70
+
+print('R$ {:.2f}'.format(taxa_ligacao))
+
